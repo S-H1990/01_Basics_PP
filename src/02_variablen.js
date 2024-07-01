d97cbabbb7b6ff3bce142637bb1ef84cf584cf64
S-H1990 <Sarahakimi206@yahoo.com>
2024-07-01 10:26:08 +0000

    02/02 Deklaration und Zuweisung

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -1,2 +1,8 @@
 // console.log("hi");
-
+// console.log(Hi);
+let firstName;//deklaration
+// console.log(firstName); //Ausgabe
+ firstName = "Sara"//wertzuweisung
+console.log(firstName); //Ausgabe
+let lastName ="sarita";
+console.log("Hi, ich bin "+firstName + " " +lastName +"!"); //Ausgabe
