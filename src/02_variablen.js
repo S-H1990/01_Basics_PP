abf71a9394b6cda22836a0e91b11a57a6c0ed99a
S-H1990 <Sarahakimi206@yahoo.com>
2024-07-01 10:51:06 +0000

    02 /02 Wertzuweisung || +Datentypen

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -1,8 +1,30 @@
 // console.log("hi");
 // console.log(Hi);
-let firstName;//deklaration
+// let firstName;//deklaration
 // console.log(firstName); //Ausgabe
- firstName = "Sara"//wertzuweisung
-console.log(firstName); //Ausgabe
-let lastName ="sarita";
-console.log("Hi, ich bin "+firstName + " " +lastName +"!"); //Ausgabe
+//  firstName = "Sara"//wertzuweisung
+// console.log(firstName); //Ausgabe
+// let lastName ="sarita";
+// console.log("Hi, ich bin "+firstName + " " +lastName +"!"); //Ausgabe
+/**Wertzuweisung || + Datentypen
+ * 
+*/
+/*undefind*/
+let test;
+// test ="hi";
+// test = 2;
+ test ="hi";//String
+console.log("Datentype: " + typeof test);//Ausgabe Datentype
+console.log("Inhalt: " + test);//Ausgabe Inhalt
+console.log("-----------")
+
+
+ test = 2;//number
+console.log("Datentype: " + typeof test);//Ausgabe Datentype
+console.log("Inhalt: " + test);//Ausgabe Inhalt
+console.log("-----------")
+
+test = true;
+console.log("Datentype: " + typeof test);//Ausgabe Datentype
+console.log("Inhalt: " + test);//Ausgabe Inhalt
+console.log("-----------")
