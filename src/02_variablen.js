1b432201bee22cc6c69acc4666a5f1fe83592c67
S-H1990 <Sarahakimi206@yahoo.com>
2024-07-01 11:11:59 +0000

    02 /02 Constanten überschreiben || Laufzeit fehler

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -10,21 +10,37 @@
  * 
 */
 /*undefind*/
-let test;
+// let test;
 // test ="hi";
 // test = 2;
- test ="hi";//String
-console.log("Datentype: " + typeof test);//Ausgabe Datentype
-console.log("Inhalt: " + test);//Ausgabe Inhalt
-console.log("-----------")
+//  test ="hi";//String
+// console.log("Datentype: " + typeof test);//Ausgabe Datentype
+// console.log("Inhalt: " + test);//Ausgabe Inhalt
+// console.log("-----------")
 
 
- test = 2;//number
-console.log("Datentype: " + typeof test);//Ausgabe Datentype
-console.log("Inhalt: " + test);//Ausgabe Inhalt
-console.log("-----------")
+//  test = 2;//number
+// console.log("Datentype: " + typeof test);//Ausgabe Datentype
+// console.log("Inhalt: " + test);//Ausgabe Inhalt
+// console.log("-----------")
 
-test = true;
-console.log("Datentype: " + typeof test);//Ausgabe Datentype
-console.log("Inhalt: " + test);//Ausgabe Inhalt
-console.log("-----------")
+// test = true;
+// console.log("Datentype: " + typeof test);//Ausgabe Datentype
+// console.log("Inhalt: " + test);//Ausgabe Inhalt
+// console.log("-----------")
+
+
+/** Variablen vs. Konstanten in JS */
+
+//Variablen
+let test;
+test = "hi";
+test = "hello";
+console.log("Inhalt: " + test);
+
+//Konstante
+const test2 = "Hi";
+// test2 = "hallo";
+console.log("Inhalt: "+ test2);
+
+
