96df7c5997bd3d00b67e6a8da1e3d1bce273d596
S-H1990 <Sarahakimi206@yahoo.com>
2024-07-02 08:26:05 +0000

    02 / 02 Änderung Variablen

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -1,46 +1,48 @@
-// console.log("hi");
-// console.log(Hi);
-// let firstName;//deklaration
-// console.log(firstName); //Ausgabe
-//  firstName = "Sara"//wertzuweisung
-// console.log(firstName); //Ausgabe
-// let lastName ="sarita";
-// console.log("Hi, ich bin "+firstName + " " +lastName +"!"); //Ausgabe
-/**Wertzuweisung || + Datentypen
- * 
-*/
-/*undefind*/
-// let test;
-// test ="hi";
-// test = 2;
-//  test ="hi";//String
-// console.log("Datentype: " + typeof test);//Ausgabe Datentype
-// console.log("Inhalt: " + test);//Ausgabe Inhalt
-// console.log("-----------")
+// // console.log("hi");
+// // console.log(Hi);
+// // let firstName;//deklaration
+// // console.log(firstName); //Ausgabe
+// //  firstName = "Sara"//wertzuweisung
+// // console.log(firstName); //Ausgabe
+// // let lastName ="sarita";
+// // console.log("Hi, ich bin "+firstName + " " +lastName +"!"); //Ausgabe
+// /**Wertzuweisung || + Datentypen
+//  * 
+// */
+// /*undefind*/
+// // let test;
+// // test ="hi";
+// // test = 2;
+// //  test ="hi";//String
+// // console.log("Datentype: " + typeof test);//Ausgabe Datentype
+// // console.log("Inhalt: " + test);//Ausgabe Inhalt
+// // console.log("-----------")
 
 
-//  test = 2;//number
-// console.log("Datentype: " + typeof test);//Ausgabe Datentype
-// console.log("Inhalt: " + test);//Ausgabe Inhalt
-// console.log("-----------")
+// //  test = 2;//number
+// // console.log("Datentype: " + typeof test);//Ausgabe Datentype
+// // console.log("Inhalt: " + test);//Ausgabe Inhalt
+// // console.log("-----------")
 
-// test = true;
-// console.log("Datentype: " + typeof test);//Ausgabe Datentype
-// console.log("Inhalt: " + test);//Ausgabe Inhalt
-// console.log("-----------")
+// // test = true;//boolean
+// // console.log("Datentype: " + typeof test);//Ausgabe Datentype
+// // console.log("Inhalt: " + test);//Ausgabe Inhalt
+// // console.log("-----------")
 
 
-/** Variablen vs. Konstanten in JS */
+// /** Variablen vs. Konstanten in JS */
 
-//Variablen
-let test;
-test = "hi";
-test = "hello";
-console.log("Inhalt: " + test);
+// // //Variablen
+// // let test;
+// // test = "hi"; //
+// // test = "hello";
+// // console.log("Inhalt: " + test);
 
-//Konstante
-const test2 = "Hi";
-// test2 = "hallo";
-console.log("Inhalt: "+ test2);
+// Konstante
+// const test2 = "Hi"; //Deklaration + Wertzuweisung gemeinsam !
+// test2 = "hallo"; // Laufzeitfehler
+// console.log("Inhalt: "+ test2); // Ausgabe
+
+// 02.07.24
 
 
