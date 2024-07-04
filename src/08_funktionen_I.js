82fd2163a8f02cf4d1351c7c5f489febf3c19af5
S-H1990 <Sarahakimi206@yahoo.com>
2024-07-04 08:20:01 +0000

    08 /01 funtion

---
+++ src/08_funktionen_I.js
@@ -0,0 +1,16 @@
+
+/***** Funktionen 01 *****/
+// 1. Kapselung von Codeblöcken
+
+
+// funtionsaufruf | call
+// test();//wenn man funtion ausschalten will dann muss man nur call auskommentieren!
+
+
+
+// funtionsrumpf | callee
+// funtionsdeklaration
+function test()
+{
+console.log("Hallo Marius! ");
+}
