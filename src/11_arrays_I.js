a57b7eac682ccd8d6de0fc6f68c82ecd61ef485b
S-H1990 <Sarahakimi206@yahoo.com>
2024-07-08 10:02:01 +0000

    11/07 Transponierung

--- src/11_arrays_I.js
+++ src/11_arrays_I.js
@@ -8,7 +8,7 @@
 
 /*** 02. Theorie: Array */
 
-let arr;
+// let arr;
 // arr = new Array();//Konstruktor
 // arr =[];// Literal
 // arr = [2,11,7,8,10];//container[],innen Elemente
@@ -37,10 +37,62 @@ let arr;
 
 
 // Inkrement (var. Schrittweite)
-for (let i = 10; i <= 100; i += 5) 
-{
-   output(i);   
+// for (let i = 10; i <= 100; i += 5) 
+// {
+//    output(i);   
+// }
+
+/* 02b. For-Schleife für Array-Index (Iteration)*/
+
+// let arr = ["Ich","bin","die","coole","Maxine","Mützerich"];
+// for (let i = 0; i < arr.length; i++) 
+// {
+// //    output(i); // Interne Index
+//    output(arr[i]); //Mapping, übertragung von internen Index auf ein exterenen Index
+// }
+/********* Überlegungen - Transponierung **********/
+
+/*
+1. Einer Variablen kann Ihr eigener Wert zugewiesen werden.
+Solange die Variable existiert, bleibt dieser erhalten.
+hier: Aufsummierung
+*/
+
+/* Zu "Fuß" - DRY !!! */
+// let a = 0; // Anfangswert
+// output("inhalt von a: " + a);
+// a = a + 1; // a = 0 + 1
+// output("inhalt von a: " + a);
+// a = a + 1; // a = 1 + 1
+// output("inhalt von a: " + a);
+// a = a + 1; // a = 2 + 1
+// output("inhalt von a: " + a);
+
+/* Besser: mit FOR-Schleife */
+
+let a = 0; // Anfangswert
+for (let i = 0; i < 10; i++) {
+//    a = a + 1
+a += 1; 
+   output("inhalt von a: " + a);
 }
+
+/*
+1a. Einer Variablen kann Ihr eigener Wert zugewiesen werden
+Solange die Variable existiert, bleibt dieser erhalten
+hier: Verkettung eines Strings // Transponierung
+*/
+
+let str = ""; // Anfangswert
+for (let i = 0; i < 10; i++) {
+   str += "Test" + " "; // a = a + 1
+   output(str);
+}
+
+
+
+
+
 
 
 
@@ -87,16 +139,36 @@ function output(outputStr) {
 // --> Transformation semantisches Problem --> numerisches Problem
 
 // output(getSentenceArr(["Ich","bin","die","coole","Maxine","Mützerich"]));   
-function getSentenceArr(arr) {
-        const GAP = " ";
-        const PUNCT = ".";
-        let str =   arr[0] + GAP +
-                    arr[1] + GAP +
-                    arr[2] + GAP +
-                    arr[3] + GAP +
-                    arr[4] + GAP +
-                    arr[5] +
-                    PUNCT;
+// function getSentenceArr(arr) {
+//         const GAP = " ";
+//         const PUNCT = ".";
+//         let str =   arr[0] + GAP +
+//                     arr[1] + GAP +
+//                     arr[2] + GAP +
+//                     arr[3] + GAP +
+//                     arr[4] + GAP +
+//                     arr[5] +
+//                     PUNCT;
     
-        return str;
-    }
+//         return str;
+//     }/*** 01b. Funktionalität mit Array 2  */
+// // Transponierung:  untereinander ---> nebeneinander
+// // Helge Schneider: Anananandereihung ...
+
+
+// output(getSentenceArr(["Ich","bin","die","coole","Maxine","Mützerich"]));
+// function getSentenceArr2(arr) {
+//     const GAP = " ";
+//     const PUNCT = ".";
+//     let str =   
+
+  
+
+
+
+// for (let i = 0; i < arr.length; i++) 
+// {
+//    output(arr[i]); 
+// }
+// return str;
+// }
